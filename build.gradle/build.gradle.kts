8897357a7676d034d7ba58b16f511a0a8f46cc48
Ghori Ghuraishi Mulyadi <ghorighuraishi07@ce.student.pens.ac.id>
2026-01-14 06:43:36 +0700

    Update build.gradle.kts

--- build.gradle/build.gradle.kts
+++ build.gradle/build.gradle.kts
@@ -97,7 +97,6 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:12.4.2.jre11")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
